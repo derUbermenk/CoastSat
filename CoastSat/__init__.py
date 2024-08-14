4ed30fb40764901560b8b09f0734426d6c2fe0ef
chester <asuncionmcv@gmail.com>
2024-08-14 14:25:39 +0800

    move rename filebased coastsat runner entities

--- CoastSat/__init__.py
+++ CoastSat/__init__.py
@@ -1,1 +1,1 @@
-from .coastSatRunner import initializeCoastSatRunner, assertfile_type_and_exists, CoastSatRunner
+from .coastSatRunner_fileBased import initializeCoastSatRunnerFileBased, assertfile_type_and_exists, CoastSatRunnerFileBased
