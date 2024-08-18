706fe509c8d1cd8935bbd2827a992974d9ef68b7
chester <asuncionmcv@gmail.com>
2024-08-18 15:59:51 +0800

    add test for querying shoreline record data from db

--- tests/test_coastSat_DB.py
+++ tests/test_coastSat_DB.py
@@ -27,6 +27,43 @@ def test_initializeCoastSatRunner():
             assert coastSatRunner.tides == tides
             assert coastSatRunner.connstring == connstring 
 
+def test_retrieve_shoreline_from_db():
+    shoreline_baseline = [
+            (1007671.020112160709687, 455030.685090125480201),
+            (1007738.149961497285403, 455136.269249796518125),
+            (1007735.60449628578499, 455475.019404369115364),
+            (1007659.86397591792047, 455679.772528785164468),
+            (1007582.069948134245351, 455898.714581696374808),
+            (1007518.585046245483682, 456095.360391221067403),
+            (1007308.360074182623066, 456271.509743563947268)
+    ]
+    shoreline_area=[ 
+        (144.79485033965136, 13.429388175797682), 
+        (144.80045079197197, 13.428688997897156), 
+        (144.78536604893787, 13.42058047237599),  
+        (144.78098868383339, 13.421999744999741)  
+    ]
+
+    expected_shoreline = Shoreline()
+    expected_shoreline.sitename="TEST1",
+    expected_shoreline.loc="test loc, testing",
+    expected_shoreline.baseline=shoreline_baseline,
+    expected_shoreline.area=shoreline_area
+
+    csRunner = CoastSatRunnerDB(
+        "2024-01-01",
+        "2024-02-01",
+        "TEST1",
+        "/data/tides.csv",
+        "postgresql://shoreline:shoreline@localhost:5436/shoreline"
+    )
+
+    shoreline = csRunner.retrieve_shoreline()
+    assert shoreline.sitename, expected_shoreline.sitename
+    assert shoreline.loc, expected_shoreline.loc
+    assert shoreline.baseline, expected_shoreline.baseline
+    assert shoreline.area, expected_shoreline.area
+
 def test_retrieve_area_geometry_coords_from_db():
     csRunner = CoastSatRunnerDB(
         "2024-01-01",
