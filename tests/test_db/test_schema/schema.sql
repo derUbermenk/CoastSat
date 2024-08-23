c7e7a3af76844c6c2a8a410e3f351025717c04f8
chester <asuncionmcv@gmail.com>
2024-08-23 09:19:19 +0800

    update test schema

--- tests/test_db/test_schema/schema.sql
+++ tests/test_db/test_schema/schema.sql
@@ -19,10 +19,11 @@ CREATE TABLE Profiles (
 ); 
 
 CREATE TABLE Transects (
-    id INT,
+    id SERIAL PRIMARY KEY,
+    transect_name VARCHAR(5) NOT NULL,
     shoreline_sitename VARCHAR(20) NOT NULL REFERENCES Shorelines(sitename) ON DELETE CASCADE,
     geom geometry(LINESTRING),
-    PRIMARY KEY(id, shoreline_sitename)
+    CONSTRAINT unique_transect_shoreline UNIQUE(transect_name, shoreline_sitename)
 );
 
 CREATE TABLE Intersects (
@@ -31,6 +32,8 @@ CREATE TABLE Intersects (
     transect_id INT NOT NULL REFERENCES Transects(id) ON DELETE CASCADE,
     distance DOUBLE PRECISION NOT NULL,
     geom geometry(POINT),
+
+    CONSTRAINT unique_id_transect_shoreline UNIQUE(id, profile_id, transect_id)
 );
 
 INSERT INTO Shorelines (sitename, baseline, area)
@@ -49,17 +52,20 @@ VALUES (
                               144.7948 13.4293))')  
 ), (
     'TEST2',
-    ST_SetSRID(
-        ST_GeomFromGeoJSON('{
-        "type": "LineString", 
-        "coordinates": [ 
-            [ -125.89586421741501, 49.110203027846936 ], 
-            [ -125.893666071682333, 49.11309892870819 ], 
-            [ -125.894848314904664, 49.118233447220312 ], 
-            [ -125.897599572756462, 49.12182124087591 ], 
-            [ -125.900544509274937, 49.123062024048529 ] 
-        ]
-        }'), 4326
+    ST_Transform(
+        ST_SetSRID(
+            ST_GeomFromGeoJSON('{
+            "type": "LineString", 
+            "coordinates": [ 
+                [ -125.89586421741501, 49.110203027846936 ], 
+                [ -125.893666071682333, 49.11309892870819 ], 
+                [ -125.894848314904664, 49.118233447220312 ], 
+                [ -125.897599572756462, 49.12182124087591 ], 
+                [ -125.900544509274937, 49.123062024048529 ] 
+            ]
+            }'), 4326
+        ),
+        3005
     ),
     ST_SetSRID(
         ST_GeomFromGeoJSON('{
@@ -73,28 +79,27 @@ VALUES (
             ] ]
         }'), 4326
     )
-)
+);
 
-INSERT INTO Transects (id, shoreline_sitename, geom)
-VALUES (
-    (1, 'TEST2', ST_SetSRID(
-            ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ 1007639.722662, 456334.414515000011306 ], [ 1006898.373719, 456033.745684999972582 ] ] }'),
-            3005
-        ),
-    ),
-    (2, 'TEST2', ST_SetSRID(
-            ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ 1007756.983505, 456045.288427 ], [ 1007015.634563, 455744.619597000011709 ] ] }'),
-            3005
-        )
-    ),
-    (3, 'TEST2', ST_SetSRID(
-        ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ 1007918.593002, 455646.813371 ], [ 1007177.244059, 455346.14454 ] ] }'),
+INSERT INTO Transects (id, transect_name, shoreline_sitename, geom)
+VALUES
+(1, 'T1', 'TEST2', ST_SetSRID(
+        ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ 1007639.722662, 456334.414515000011306 ], [ 1006898.373719, 456033.745684999972582 ] ] }'),
         3005
-        )
-    ),
-    (4, 'TEST2', ST_SetSRID(
-        ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ 1008108.766037, 455177.910164 ], [ 1007367.417094, 454877.241334000020288 ] ] }'),
-        3005
-        )
     )
-)
+),
+(2, 'T2', 'TEST2', ST_SetSRID(
+        ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ 1007756.983505, 456045.288427 ], [ 1007015.634563, 455744.619597000011709 ] ] }'),
+        3005
+    )
+),
+(3, 'T3', 'TEST2', ST_SetSRID(
+    ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ 1007918.593002, 455646.813371 ], [ 1007177.244059, 455346.14454 ] ] }'),
+    3005
+    )
+),
+(4, 'T4', 'TEST2', ST_SetSRID(
+    ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ 1008108.766037, 455177.910164 ], [ 1007367.417094, 454877.241334000020288 ] ] }'),
+    3005
+    )
+);
