cb2f1011c1f32b9a5040f0e94b86320d4e51cc0c
chester <asuncionmcv@gmail.com>
2024-08-28 15:32:41 +0800

    update schema: add shoreline_sitename foreign key on intersects table

--- tests/test_db/test_schema/schema.sql
+++ tests/test_db/test_schema/schema.sql
@@ -38,6 +38,7 @@ CREATE TABLE Intersects (
     id SERIAL PRIMARY KEY,
     profile_record_date DATE NOT NULL REFERENCES Profiles(record_date) ON DELETE CASCADE,
     transect_id INT NOT NULL REFERENCES Transects(id) ON DELETE CASCADE,
+    shoreline_sitename VARCHAR(20) NOT NULL REFERENCES Shorelines(sitename) ON DELETE CASCADE,
     distance DOUBLE PRECISION NOT NULL,
     geom geometry(POINT),
 
