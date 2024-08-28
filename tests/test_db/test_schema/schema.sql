55235f6cc61621217d0b21f26f1b33f68a8fdba8
chester <asuncionmcv@gmail.com>
2024-08-28 12:37:06 +0800

    make id column of intersects table primary key and serial type

--- tests/test_db/test_schema/schema.sql
+++ tests/test_db/test_schema/schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE Transects (
 );
 
 CREATE TABLE Intersects (
-    id INT,
+    id SERIAL PRIMARY KEY,
     profile_record_date DATE NOT NULL REFERENCES Profiles(record_date) ON DELETE CASCADE,
     transect_id INT NOT NULL REFERENCES Transects(id) ON DELETE CASCADE,
     distance DOUBLE PRECISION NOT NULL,
